218d86789d16740927b3a576b25f6de98edb75f6
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-10-07 11:55:42 -0600

    debugging after_install

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -1,11 +1,13 @@
 #!bin/bash
 
 cd /home/ubuntu/django_app
-python3 -m virtualenv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install django
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
+mkdir test
+cd test
+# python3 -m virtualenv venv
+# source venv/bin/activate
+# pip3 install -r requirements.txt
+# pip3 install django
+# python3 manage.py migrate
+# python3 manage.py collectstatic --no-input
 
-sudo service apache2 restart
+# sudo service apache2 restart
