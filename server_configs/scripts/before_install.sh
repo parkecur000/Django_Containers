8bd41a7085df4839c0029a631d9a8d2be12f7026
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-10-07 12:29:29 -0600

    tests

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "TEST"
+cd /home/ubuntu
+touch test.txt
 DIR="/home/ubuntu/django_app"
 if [-d "$DIR"]; then
     echo "$DIR exists"
@@ -6,8 +9,7 @@ else
     echo "Creating $DIR directory"
     mkdir $DIR
 fi
-cd $DIR
-touch test.txt
+
 sudo apt-get update
 sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 libmysqlclient-dev
 sudo pip3 install virtualenv
