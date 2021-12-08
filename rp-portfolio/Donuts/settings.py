671964362840f26636f52d50ae26a81343a2a600
parkecur000 <curtmparker@gmail.com>
2021-12-08 13:11:10 -0700

    Take out database password and host url to make github repo public for grading

--- rp-portfolio/Donuts/settings.py
+++ rp-portfolio/Donuts/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME' : 'donuts',
         'USER' : 'admin',
-        'PASSWORD' : PasswordRemoved,
-        'HOST' : Host URL Removed for Grading,        
+        'PASSWORD' : #PasswordRemoved,
+        'HOST' : #Host URL Removed for Grading,        
         'PORT' : '3306',
         'OPTIONS' : {
             'charset' : 'utf8mb4'
